4a5a055eb733734be49e70d5e2ca8cb9e391f530
Fazle Bari <fazlebarisn@gmail.com>
2023-07-14 18:01:50 +0600

    lern

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+function Hello(){
+    return(
+        <>
+            <p>My Name is Sony!</p>
+            <input type="text"/>
+            <div>
+                <h1>The Man</h1>
+            </div>
+        </>
+    );
+}
+
+export default Hello;
